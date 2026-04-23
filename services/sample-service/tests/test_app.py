ef5729ab4cf30f066e5206052f5fd01368cac616
Muhammad Imad Ul Islam <232082@students.au.edu.pk>
2026-04-24 01:10:40 +0500

    Initial commit of workspace files

--- services/sample-service/tests/test_app.py
+++ services/sample-service/tests/test_app.py
@@ -1,4 +1,8 @@
-from services.sample_service.app import app as flask_app
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app as flask_app
 
 
 def test_index_response():
